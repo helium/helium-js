028cb76e25f8676506430d30fb9391c82b2f5998
Matt Wilhelm <AddressXception@users.noreply.github.com>
2020-07-17 18:48:04 -0400

    Add getPriceAtBlock and getPredictedPrice (#64)
    Address PR Feedback
    
    
    fix balance file
    
    
    missed one

--- packages/http/src/resources/Oracle.ts
+++ packages/http/src/resources/Oracle.ts
@@ -10,9 +10,27 @@ export default class Oracle {
   }
 
   async getCurrentPrice(): Promise<any> {
-    const url = `/oracle/prices/current`
-    const { data: { data: { price } } } = await this.client.get(url)
-    
-    return new Balance(price, CurrencyType.usd)
+    const url = '/oracle/prices/current'
+    const { data: { data: { price, block } } } = await this.client.get(url)
+
+    return { price: new Balance(price, CurrencyType.usd), height: block }
+  }
+
+  async getPriceAtBlock(height: number): Promise<any> {
+    const url = `/oracle/prices/${height}`
+    const { data: { data: { price, block } } } = await this.client.get(url)
+
+    return { price: new Balance(price, CurrencyType.usd), height: block }
+  }
+
+  async getPredictedPrice(): Promise<any> {
+    const url = '/oracle/predictions'
+    const response = await this.client.get(url)
+    if (!response.data.data || !response.data.data.length) {
+      return []
+    }
+
+    const { data: { data: [{ price, time }] } } = response
+    return { price: new Balance(price, CurrencyType.usd), time }
   }
 }
--- packages/http/src/resources/__tests__/Oracle.spec.ts
+++ packages/http/src/resources/__tests__/Oracle.spec.ts
@@ -14,6 +14,84 @@ describe('current oracle price', () => {
   it('gets the current price', async () => {
     const client = new Client()
     const price = await client.oracle.getCurrentPrice()
-    expect(price.integerBalance).toBe(42040700)
+    expect(price.price.integerBalance).toBe(42040700)
+    expect(price.height).toBe(385010)
+  })
+})
+
+describe('oracle price at block', () => {
+  nock('https://api.helium.io')
+    .get('/v1/oracle/prices/385010')
+    .reply(200, {
+      data: {
+        price: 42040700,
+        block: 385010,
+      },
+    })
+
+  it('gets the price at the specified block', async () => {
+    const client = new Client()
+    const price = await client.oracle.getPriceAtBlock(385010)
+    expect(price.price.integerBalance).toBe(42040700)
+    expect(price.height).toBe(385010)
+  })
+})
+
+describe('oracle predicted price exists', () => {
+  nock('https://api.helium.io')
+    .get('/v1/oracle/predictions')
+    .reply(200, {
+      data: [
+        {
+          price: 42040700,
+          time: 1594410146,
+        }],
+    })
+
+  it('gets the oracle price prediction', async () => {
+    const client = new Client()
+    const price = await client.oracle.getPredictedPrice()
+    expect(price.price.integerBalance).toBe(42040700)
+    expect(price.time).toBe(1594410146)
+  })
+})
+
+describe('oracle predicted price has multiple values', () => {
+  nock('https://api.helium.io')
+    .get('/v1/oracle/predictions')
+    .reply(200, {
+      data: [
+        {
+          price: 42040700,
+          time: 1594410146,
+        },
+        {
+          price: 42040701,
+          time: 1594410147,
+        }],
+    })
+
+  it('gets the first oracle price prediction', async () => {
+    const client = new Client()
+    const price = await client.oracle.getPredictedPrice()
+    expect(price.price.integerBalance).toBe(42040700)
+    expect(price.time).toBe(1594410146)
+  })
+})
+
+describe('oracle predicted price is empty', () => {
+  nock('https://api.helium.io')
+    .get('/v1/oracle/predictions')
+    .reply(200, {
+      data: [],
+    })
+
+  it('gets a response with empty data', async () => {
+    const client = new Client()
+    const price = await client.oracle.getPredictedPrice()
+    expect(price).toBeInstanceOf(Array)
+    expect(price.length).toBe(0)
+    expect(price.price).toBeUndefined()
+    expect(price.time).toBeUndefined()
   })
 })
