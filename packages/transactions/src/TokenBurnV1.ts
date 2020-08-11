fbddbaa9330989fb1ae523d05b584010b39ba627
Tyler Whitman <twhit093@gmail.com>
2020-08-11 12:01:09 -0700

    add TokenBurnV1 transaction

---
+++ packages/transactions/src/TokenBurnV1.ts
@@ -0,0 +1,72 @@
+import proto from '@helium/proto'
+import Transaction from './Transaction'
+import { toUint8Array, EMPTY_SIGNATURE } from './utils'
+import { Addressable, SignableKeypair } from './types'
+
+interface TokenBurnOptions {
+  payer?: Addressable
+  payee?: Addressable
+  amount?: number
+  nonce?: number
+  memo?: number
+}
+
+interface SignOptions {
+  payer: SignableKeypair
+}
+
+export default class TokenBurnV1 extends Transaction {
+  public payer?: Addressable
+  public payee?: Addressable
+  public amount?: number
+  public nonce?: number
+  public signature?: Uint8Array
+  public fee?: number
+  public memo?: number
+
+  constructor(opts: TokenBurnOptions) {
+    super()
+    this.payer = opts.payer
+    this.payee = opts.payee
+    this.amount = opts.amount
+    this.nonce = opts.nonce
+    this.memo = opts.memo
+    this.fee = this.calculateFee()
+  }
+
+  serialize(): Uint8Array {
+    const BlockchainTxn = proto.helium.blockchain_txn
+
+    const tokenBurn = this.toProto()
+    const txn = BlockchainTxn.create({ tokenBurn })
+    return BlockchainTxn.encode(txn).finish()
+  }
+
+  async sign({ payer: payerKeypair }: SignOptions): Promise<TokenBurnV1> {
+    const TokenBurnTxn = proto.helium.blockchain_txn_token_burn_v1
+    const tokenBurn = this.toProto(true)
+    const serialized = TokenBurnTxn.encode(tokenBurn).finish()
+    const signature = await payerKeypair.sign(serialized)
+    this.signature = signature
+    return this
+  }
+
+  private toProto(forSigning: boolean = false): proto.helium.blockchain_txn_token_burn_v1 {
+    const TokenBurnTxn = proto.helium.blockchain_txn_token_burn_v1
+    return TokenBurnTxn.create({
+      payer: this.payer ? toUint8Array(this.payer.bin) : null,
+      payee: this.payee ? toUint8Array(this.payee.bin) : null,
+      amount: this.amount,
+      nonce: this.nonce,
+      signature: this.signature && !forSigning ? toUint8Array(this.signature) : null,
+      fee: this.fee,
+      memo: this.memo,
+    })
+  }
+
+  calculateFee(): number {
+    this.signature = EMPTY_SIGNATURE
+    const payload = this.serialize()
+    return Transaction.calculateFee(payload)
+  }
+}
---
+++ packages/transactions/src/__tests__/TokenBurnV1.spec.ts
@@ -0,0 +1,71 @@
+import proto from '@helium/proto'
+import { TokenBurnV1, Transaction } from '..'
+import {
+  usersFixture,
+  bobB58,
+  aliceB58,
+} from '../../../../integration_tests/fixtures/users'
+
+Transaction.config({
+  txnFeeMultiplier: 5000,
+  dcPayloadSize: 24,
+  stakingFeeTxnAddGatewayV1: 40 * 100000,
+  stakingFeeTxnAssertLocationV1: 10 * 100000,
+})
+
+const tokenBurnFixture = async () => {
+  const { bob, alice } = await usersFixture()
+
+  return new TokenBurnV1({
+    payer: bob.address,
+    payee: alice.address,
+    amount: 10,
+    nonce: 1,
+    memo: 1234567890,
+  })
+}
+
+test('create a token burn txn', async () => {
+  const tokenBurn = await tokenBurnFixture()
+  expect(tokenBurn.payer?.b58).toBe(bobB58)
+  expect(tokenBurn.payee?.b58).toBe(aliceB58)
+  expect(tokenBurn.amount).toBe(10)
+  expect(tokenBurn.nonce).toBe(1)
+  expect(tokenBurn.fee).toBe(35000)
+  expect(tokenBurn.memo).toBe(1234567890)
+})
+
+describe('serialize', () => {
+  it('serializes a payment txn', async () => {
+    const tokenBurn = await tokenBurnFixture()
+    expect(tokenBurn.serialize().length).toBeGreaterThan(0)
+  })
+
+  it('serializes to base64 string', async () => {
+    const tokenBurn = await tokenBurnFixture()
+    const burnString = tokenBurn.toString()
+    // verify that we can decode it back from its serialized string
+    const buf = Buffer.from(burnString, 'base64')
+    const decoded = proto.helium.blockchain_txn.decode(buf)
+    expect(decoded.tokenBurn?.amount.toNumber()).toBe(10)
+  })
+})
+
+describe('sign', () => {
+  it('adds the payer signature', async () => {
+    const { bob, alice } = await usersFixture()
+    const tokenBurn = new TokenBurnV1({
+      payer: bob.address,
+      payee: alice.address,
+      amount: 10,
+      nonce: 1,
+      memo: 1234567890,
+    })
+
+    const signedPayment = await tokenBurn.sign({ payer: bob })
+
+    if (!signedPayment.signature) throw new Error('null')
+
+    expect(Buffer.byteLength(Buffer.from(signedPayment.signature))).toBe(64)
+  })
+})
