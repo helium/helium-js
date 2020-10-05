d4eefe767fc8a4a2dcd2936ae45035214fdd4c54
Tyler Whitman <twhit093@gmail.com>
2020-10-05 16:56:07 -0700

    update challenge data for poc v10 and result data

--- packages/http/src/models/Challenge.ts
+++ packages/http/src/models/Challenge.ts
@@ -1,10 +1,8 @@
-import camelcaseKeys from 'camelcase-keys'
-
 export interface HTTPChallengeObject {
   type: string
   time: number
-  signature: string
   secret: string
+  request_block_hash: string
   path: HTTPPathObject[]
   onion_key_hash: string
   height: number
@@ -12,60 +10,194 @@ export interface HTTPChallengeObject {
   fee: number
   challenger_owner: string
   challenger_lon: number
-  challenger_loc: string
+  challenger_location: string
   challenger_lat: number
   challenger: string
 }
 
 interface HTTPPathObject {
-  challengee: string
   witnesses: HTTPWitnessesObject[]
   receipt: HTTPReceiptObject
+  geocode: HTTPGeocodeObject
+  challengee_owner: string
+  challengee_lon: string
+  challengee_location: string
+  challengee_lat: string
+  challengee: string
+}
+
+interface HTTPGeocodeObject {
+  short_street: string
+  short_state: string
+  short_country: string
+  short_city: string
+  long_street: string
+  long_state: string
+  long_country: string
+  long_city: string
+  city_id: string
 }
 
 interface HTTPWitnessesObject {
   timestamp: number
+  snr: number
   signal: number
   packet_hash: string
+  owner: string
+  location: string
+  is_valid: boolean
   gateway: string
+  frequency: number
+  datarate: number[]
+  channel: number
 }
 
 interface HTTPReceiptObject {
   timestamp: number
-  signature: string
+  snr: number
   signal: number
   origin: string
   gateway: string
+  frequency: number
+  datarate: number[]
   data: string
+  channel: number
 }
 
-interface Path {
-  challengee: string
+export interface Geocode {
+  shortStreet: string
+  shortState: string
+  shortCountry: string
+  shortCity: string
+  longStreet: string
+  longState: string
+  longCountry: string
+  longCity: string
+  cityId: string
+}
+
+export enum PathResult {
+  SUCCESS = 'success',
+  FAILURE = 'failure',
+  UNTESTED = 'untested',
+}
+
+export interface Path {
   witnesses: Witness[]
   receipt: Receipt
+  geocode: Geocode
+  challengeeOwner: string
+  challengeeLon: string
+  challengeeLocation: string
+  challengeeLat: string
+  challengee: string
+  result: PathResult
 }
 
-interface Witness {
+export interface Witness {
   timestamp: number
+  snr: number
   signal: number
   packetHash: string
+  owner: string
+  location: string
+  isValid: boolean
   gateway: string
+  frequency: number
+  datarate: number[]
+  channel: number
 }
 
-interface Receipt {
+export interface Receipt {
   timestamp: number
-  signature: string
+  snr: number
   signal: number
   origin: string
   gateway: string
+  frequency: number
+  datarate: number[]
   data: string
+  channel: number
+}
+
+const constructPath = (path: HTTPPathObject[]): Path[] => {
+  let hasFailedPath = false
+  const isBeacon = path.length === 1
+  return path.map((pathObject, i) => {
+    const hasReceipt = pathObject.receipt
+    const hasGeocode = pathObject.geocode
+    const hasWitness = pathObject.witnesses.length > 0
+    const hasReceiptOrWitnesses = hasReceipt || hasWitness
+    const hasReceiptAndWitnesses = hasReceipt && hasWitness
+    const nextElement = path[i + 1]
+    const nextElementHasReceiptOrWitness = nextElement && (nextElement.receipt
+        || nextElement.witnesses.length > 0)
+    const isFirstElement = i === 0
+    const isValidBeacon = isBeacon && hasReceipt && hasWitness
+    const isValidChallenge = !isBeacon && (
+      isFirstElement
+        ? (hasReceiptAndWitnesses || nextElementHasReceiptOrWitness)
+        : (hasReceiptOrWitnesses || nextElementHasReceiptOrWitness)
+    )
+    const isFailure = isFirstElement ? !hasWitness : !hasReceipt && !hasWitness
+    let result: PathResult = PathResult.UNTESTED
+    if (!hasFailedPath && (isValidBeacon || isValidChallenge)) {
+      result = PathResult.SUCCESS
+    } else if (!hasFailedPath && isFailure) {
+      result = PathResult.FAILURE
+      hasFailedPath = true
+    }
+    return {
+      witnesses: hasWitness ? pathObject.witnesses.map((witness) => ({
+        timestamp: witness.timestamp,
+        snr: witness.snr,
+        signal: witness.signal,
+        packetHash: witness.packet_hash,
+        owner: witness.owner,
+        location: witness.location,
+        isValid: witness.is_valid,
+        gateway: witness.gateway,
+        frequency: witness.frequency,
+        datarate: witness.datarate,
+        channel: witness.channel,
+      } as Witness)) as Witness[] : [],
+      receipt: hasReceipt ? {
+        timestamp: pathObject.receipt.timestamp,
+        snr: pathObject.receipt.snr,
+        signal: pathObject.receipt.signal,
+        origin: pathObject.receipt.origin,
+        gateway: pathObject.receipt.gateway,
+        frequency: pathObject.receipt.frequency,
+        datarate: pathObject.receipt.datarate,
+        data: pathObject.receipt.data,
+        channel: pathObject.receipt.channel,
+      } as Receipt : undefined,
+      geocode: hasGeocode ? {
+        shortStreet: pathObject.geocode.short_street,
+        shortState: pathObject.geocode.short_state,
+        shortCountry: pathObject.geocode.short_country,
+        shortCity: pathObject.geocode.short_city,
+        longStreet: pathObject.geocode.long_street,
+        longState: pathObject.geocode.long_state,
+        longCountry: pathObject.geocode.long_country,
+        longCity: pathObject.geocode.long_city,
+        cityId: pathObject.geocode.city_id,
+      } as Geocode : undefined,
+      challengeeOwner: pathObject.challengee_owner,
+      challengeeLon: pathObject.challengee_lon,
+      challengeeLocation: pathObject.challengee_location,
+      challengeeLat: pathObject.challengee_lat,
+      challengee: pathObject.challengee,
+      result,
+    } as Path
+  })
 }
 
 export default class Challenge {
   public type: string
   public time: number
-  public signature: string
   public secret: string
+  public requestBlockHash: string
   public path: Path[]
   public onionKeyHash: string
   public height: number
@@ -73,23 +205,23 @@ export default class Challenge {
   public fee: number
   public challengerOwner: string
   public challengerLon: number
-  public challengerLoc: string
+  public challengerLocation: string
   public challengerLat: number
   public challenger: string
 
   constructor(challenge: HTTPChallengeObject) {
     this.type = challenge.type
     this.time = challenge.time
-    this.signature = challenge.signature
     this.secret = challenge.secret
-    this.path = camelcaseKeys(challenge.path) as any
+    this.requestBlockHash = challenge.request_block_hash
+    this.path = constructPath(challenge.path)
     this.onionKeyHash = challenge.onion_key_hash
     this.height = challenge.height
     this.hash = challenge.hash
     this.fee = challenge.fee
     this.challengerOwner = challenge.challenger_owner
     this.challengerLon = challenge.challenger_lon
-    this.challengerLoc = challenge.challenger_loc
+    this.challengerLocation = challenge.challenger_location
     this.challengerLat = challenge.challenger_lat
     this.challenger = challenge.challenger
   }
