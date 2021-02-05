d3d7ce17d0271719c3a149f3602b41334b5e49b4
Tyler Whitman <twhit093@gmail.com>
2021-02-04 16:12:47 -0800

    add reward to export

--- packages/http/src/index.ts
+++ packages/http/src/index.ts
@@ -9,6 +9,7 @@ export { BlockData as Block } from './models/Block'
 export { CityData as City } from './models/City'
 export { ElectionData as Election } from './models/Election'
 export { SumData as Sum } from './models/Sum'
+export { RewardData as Reward } from './models/Reward'
 export { PendingTransactionData as PendingTransaction } from './models/PendingTransaction'
 export {
   AnyTransaction,
