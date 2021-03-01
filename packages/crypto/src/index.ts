7389de9ca2a1164bb78b9addb6c06714b3d2cc7b
Andrew Allen <allenan@helium.com>
2021-03-01 13:10:18 -0800

    add exports to package index

--- packages/crypto/src/index.ts
+++ packages/crypto/src/index.ts
@@ -1,4 +1,6 @@
 export { default as Mnemonic } from './Mnemonic'
 export { default as Keypair } from './Keypair'
 export { default as Address } from './Address'
+export * as NetType from './NetType'
+export * as KeyType from './KeyType'
 export * as utils from './utils'
