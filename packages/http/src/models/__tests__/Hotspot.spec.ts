407233db264546caa84b91237296e9bb2dae6b18
Daniel James <danielcolinjames@gmail.com>
2021-07-13 13:52:35 -0700

    Add mode to hotspot object for data-only support (#194)
    * Add mode to hotspot object for data-only support
    
    * Add test for mode field
    
    Co-authored-by: Andrew Allen <allenan@helium.com>

--- packages/http/src/models/__tests__/Hotspot.spec.ts
+++ packages/http/src/models/__tests__/Hotspot.spec.ts
@@ -36,6 +36,7 @@ test('create Hotspot from HTTP response', () => {
     },
     gain: 12,
     elevation: 0,
+    mode: 'full',
     block_added: 123456,
     block: 123456,
     address: 'mock-address',
@@ -47,5 +48,6 @@ test('create Hotspot from HTTP response', () => {
   expect(hotspot.geocode?.longCity).toBe('mock-long-city')
   expect(hotspot.blockAdded).toBe(123456)
   expect(hotspot.locationHex).toBe('mock-location-hex')
+  expect(hotspot.mode).toBe('full')
   expect(hotspot.status?.timestamp).toBe('2021-07-13T05:31:11.562000Z')
 })
