3c5017a525920991df715e873b07361db6bb3b6b
vulet <spghtz@gmail.com>
2021-07-14 22:41:49 +0800

    styling(crypto): semicolon

--- packages/crypto/src/__tests__/Utils.spec.ts
+++ packages/crypto/src/__tests__/Utils.spec.ts
@@ -7,7 +7,7 @@ import { MAINNET } from '../NetType'
 describe('deriveChecksumBits', () => {
   it('should generate a bip39 checksum from entropy', async () => {
     const entropy = '00000000000000000000000000000000'
-    const entropyBuffer = Buffer.from(entropy, 'hex');
+    const entropyBuffer = Buffer.from(entropy, 'hex')
     const derivedChecksumBits = utils.deriveChecksumBits(entropyBuffer)
     expect(derivedChecksumBits).toStrictEqual('0011')
   })
