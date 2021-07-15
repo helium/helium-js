edfa6064bee7c91614c41a9f77597f1286dd452c
Andrew Allen <allenan@helium.com>
2021-07-15 10:31:16 -0700

    Validator model

---
+++ packages/http/src/models/Validator.ts
@@ -0,0 +1,105 @@
+import Balance, { CurrencyType, NetworkTokens } from '../../../currency/build'
+import type Client from '../Client'
+import DataModel from './DataModel'
+// import Transactions from '../resources/Transactions'
+// import Rewards from '../resources/Rewards'
+
+export type ValidatorData = Omit<Validator, 'client'>
+
+export type Bucket = 'hour' | 'day' | 'week'
+
+export type NaturalDate = string // in the format "-${number} ${Bucket}" eg "-1 day"
+
+export interface HTTPValidatorObject {
+  version_heartbeat: number
+  status: {
+    height: number
+    online: string
+    listen_addrs: string[]
+  }
+  stake_status: string
+  stake: number
+  penalty: number
+  penalties: Penalty[]
+  owner: string
+  name: string
+  last_heartbeat: number
+  block_added: number
+  block: number
+  address: string
+}
+
+export interface Penalty {
+  type: string
+  height: number
+  amount: number
+}
+
+export interface Status {
+  height: number
+  online: string
+  listenAddrs: string[]
+}
+
+export default class Validator extends DataModel {
+  private client: Client
+
+  public versionHeartbeat: number
+
+  public status: Status
+
+  public stakeStatus: string
+
+  public stake: Balance<NetworkTokens>
+
+  public penalty: number
+
+  public penalties: Penalty[]
+
+  public owner: string
+
+  public name: string
+
+  public lastHeartbeat: number
+
+  public blockAdded: number
+
+  public block: number
+
+  public address: string
+
+  constructor(client: Client, validator: HTTPValidatorObject) {
+    super()
+    this.client = client
+    this.versionHeartbeat = validator.version_heartbeat
+    this.status = {
+      height: validator.status?.height || 0,
+      online: validator.status?.online || '',
+      listenAddrs: validator.status?.listen_addrs || [],
+    }
+    this.stakeStatus = validator.stake_status
+    this.stake = new Balance(validator.stake, CurrencyType.networkToken)
+    this.penalty = validator.penalty
+    this.penalties = validator.penalties
+    this.owner = validator.owner
+    this.name = validator.name
+    this.lastHeartbeat = validator.last_heartbeat
+    this.blockAdded = validator.block_added
+    this.block = validator.block
+    this.address = validator.address
+  }
+
+  // public get activity(): Transactions {
+  //   return new Transactions(this.client, this)
+  // }
+
+  // public get rewards(): Rewards {
+  //   return new Rewards(this.client, this)
+  // }
+
+  get data(): ValidatorData {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { client, ...rest } = this
+    return { ...rest }
+  }
+}
---
+++ packages/http/src/models/__tests__/Validator.spec.ts
@@ -0,0 +1,40 @@
+import Client from '../../Client'
+import Validator from '../Validator'
+
+const mockHTTPValidator = {
+  version_heartbeat: 10000008,
+  status: { online: 'online', listen_addrs: ['/ip4/11.111.111.111/tcp/2154'], height: 923303 },
+  stake_status: 'staked',
+  stake: 1000000000000,
+  penalty: 1.123,
+  penalties: [
+    { type: 'performance', height: 921773, amount: 0.099 },
+    { type: 'tenure', height: 921622, amount: 0.5 },
+  ],
+  owner: 'fake-owner-address',
+  name: 'fake-name',
+  last_heartbeat: 923338,
+  block_added: 123456,
+  block: 923409,
+  address: 'fake-address',
+}
+
+test('create Hotspot from HTTP response', () => {
+  const validator = new Validator(new Client(), mockHTTPValidator)
+  expect(validator.versionHeartbeat).toBe(10000008)
+  expect(validator.status.online).toBe('online')
+  expect(validator.status.listenAddrs[0]).toBe('/ip4/11.111.111.111/tcp/2154')
+  expect(validator.status.height).toBe(923303)
+  expect(validator.stakeStatus).toBe('staked')
+  expect(validator.stake.integerBalance).toBe(1000000000000)
+  expect(validator.penalty).toBe(1.123)
+  expect(validator.penalties[0].type).toBe('performance')
+  expect(validator.penalties[0].height).toBe(921773)
+  expect(validator.penalties[0].amount).toBe(0.099)
+  expect(validator.owner).toBe('fake-owner-address')
+  expect(validator.name).toBe('fake-name')
+  expect(validator.lastHeartbeat).toBe(923338)
+  expect(validator.blockAdded).toBe(123456)
+  expect(validator.block).toBe(923409)
+  expect(validator.address).toBe('fake-address')
+})
