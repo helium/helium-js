f86bd32afcd3082fee80a7cb415dd98ea62f2522
Andrew Allen <allenan@helium.com>
2021-08-13 14:41:33 -0700

    refactor

--- packages/http/src/resources/Transactions.ts
+++ packages/http/src/resources/Transactions.ts
@@ -13,16 +13,6 @@ interface ListParams {
   filterTypes?: Array<string>
 }
 
-function transactionsUrlFromBlock(block: Block): string {
-  if (block.height) {
-    return `/blocks/${block.height}/transactions`
-  }
-  if (block.hash) {
-    return `/blocks/hash/${block.hash}/transactions`
-  }
-  throw new Error('Block must have either height or hash')
-}
-
 type Context = Block | Account | Hotspot | Validator
 
 export default class Transactions {
@@ -48,55 +38,35 @@ export default class Transactions {
   }
 
   async list(params: ListParams = {}): Promise<ResourceList<AnyTransaction>> {
+    let url
+
     if (this.context instanceof Block) {
-      return this.listFromBlock(params)
+      const block = this.context as Block
+      if (block.height) {
+        url = `/blocks/${block.height}/transactions`
+      } else if (block.hash) {
+        url = `/blocks/hash/${block.hash}/transactions`
+      } else {
+        throw new Error('Block must have either height or hash')
+      }
+    } else if (this.context instanceof Account) {
+      const account = this.context as Account
+      url = `/accounts/${account.address}/activity`
+    } else if (this.context instanceof Hotspot) {
+      const hotspot = this.context as Hotspot
+      url = `/hotspots/${hotspot.address}/activity`
+    } else if (this.context instanceof Validator) {
+      const validator = this.context as Validator
+      url = `/validators/${validator.address}/activity`
+    } else {
+      throw new Error('Must provide a context to list transactions from')
     }
-    if (this.context instanceof Account) {
-      return this.listFromAccount(params)
-    }
-    if (this.context instanceof Hotspot) {
-      return this.listFromHotspot(params)
-    }
-    if (this.context instanceof Validator) {
-      return this.listFromValidator(params)
-    }
-    throw new Error('Must provide a block, account or hotspot to list transactions from')
-  }
+    console.log('url will be', url)
 
-  private async listFromBlock(params: ListParams): Promise<ResourceList<AnyTransaction>> {
-    const block = this.context as Block
-    const url = transactionsUrlFromBlock(block)
-    const response = await this.client.get(url, { cursor: params.cursor })
-    const { data: { data: txns, cursor } } = response
-    const data = txns.map((d: TxnJsonObject) => Transaction.fromJsonObject(d))
-    return new ResourceList(data, this.list.bind(this), cursor)
-  }
-
-  private async listFromAccount(params: ListParams): Promise<ResourceList<AnyTransaction>> {
-    const account = this.context as Account
-    const url = `/accounts/${account.address}/activity`
+    // eslint-disable-next-line @typescript-eslint/naming-convention
     const filter_types = params.filterTypes ? params.filterTypes.join() : undefined
     const response = await this.client.get(url, { cursor: params.cursor, filter_types })
-    const { data: { data: txns, cursor } } = response
-    const data = txns.map((d: TxnJsonObject) => Transaction.fromJsonObject(d))
-    return new ResourceList(data, this.list.bind(this), cursor)
-  }
-
-  private async listFromHotspot(params: ListParams): Promise<ResourceList<AnyTransaction>> {
-    const hotspot = this.context as Hotspot
-    const url = `/hotspots/${hotspot.address}/activity`
-    const filter_types = params.filterTypes ? params.filterTypes.join() : undefined
-    const response = await this.client.get(url, { cursor: params.cursor, filter_types })
-    const { data: { data: txns, cursor } } = response
-    const data = txns.map((d: TxnJsonObject) => Transaction.fromJsonObject(d))
-    return new ResourceList(data, this.list.bind(this), cursor)
-  }
-
-  private async listFromValidator(params: ListParams): Promise<ResourceList<AnyTransaction>> {
-    const validator = this.context as Validator
-    const url = `/validators/${validator.address}/activity`
-    const filter_types = params.filterTypes ? params.filterTypes.join() : undefined
-    const response = await this.client.get(url, { cursor: params.cursor, filter_types })
+    console.log('resposne', response)
     const { data: { data: txns, cursor } } = response
     const data = txns.map((d: TxnJsonObject) => Transaction.fromJsonObject(d))
     return new ResourceList(data, this.list.bind(this), cursor)
