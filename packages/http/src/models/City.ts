600e7835eda14c42457cace9117aba4d4100e7e9
Daniel James <danielcolinjames@gmail.com>
2021-08-17 15:58:01 -0700

    Add cities.get('city-id') (#224)
    * Add cities.get('city-id')
    - also add online / offline hotspot counts to cities
    
    * Fix test

--- packages/http/src/models/City.ts
+++ packages/http/src/models/City.ts
@@ -10,6 +10,8 @@ export interface HTTPCityObject {
   long_country?: string
   long_city?: string
   hotspot_count?: number
+  offline_count?: number
+  online_count?: number
   city_id: string
 }
 
@@ -32,6 +34,10 @@ export default class City extends DataModel {
 
   public hotspotCount?: number
 
+  public onlineCount?: number
+
+  public offlineCount?: number
+
   public cityId: string
 
   constructor(client: Client, data: HTTPCityObject) {
@@ -44,6 +50,8 @@ export default class City extends DataModel {
     this.longCountry = data.long_country
     this.longCity = data.long_city
     this.hotspotCount = data.hotspot_count
+    this.onlineCount = data.online_count
+    this.offlineCount = data.offline_count
     this.cityId = data.city_id
   }
 
