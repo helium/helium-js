8f3b1d31236f32796fc2e3612589ef47eee63fd2
Matt Reetz <matthewcarlreetz@gmail.com>
2021-10-28 12:46:03 -0500

    Test default client headers.

--- packages/http/src/__tests__/Client.spec.ts
+++ packages/http/src/__tests__/Client.spec.ts
@@ -1,6 +1,5 @@
 import nock from 'nock'
-import Client from '..'
-import { Network } from '../'
+import Client, { Network } from '..'
 
 test('exposes a client instance with default options', () => {
   const prodUrl = 'https://api.helium.io'
@@ -51,6 +50,26 @@ describe('get', () => {
   })
 })
 
+it('client passes default headers to GET request', async () => {
+  const nameHeader = 'name-header-test'
+  const userAgentHeader = 'user-agent-test'
+  const network = 'helium'
+  nock('https://api.helium.io')
+    .get('/v1/greeting')
+    .reply(200, {
+      greeting: 'hello',
+    })
+
+  const opts = { name: nameHeader, userAgent: userAgentHeader, headers: { network } }
+  const client = new Client(Network.production, opts)
+  const response = await client.get('/greeting')
+
+  expect(response.data.greeting).toBe('hello')
+  expect(response.config.headers['x-client-name']).toBe(nameHeader)
+  expect(response.config.headers['User-Agent']).toBe(userAgentHeader)
+  expect(response.config.headers.network).toBe(network)
+})
+
 describe('post', () => {
   it('creates a POST request to the full url', async () => {
     nock('https://api.helium.io').post('/v1/greeting', { greeting: 'hello' }).reply(200, {
@@ -76,6 +95,24 @@ describe('post', () => {
     expect(response.data.response).toBe('hey there!')
     expect(response.config.headers['x-client-name']).toBe(nameHeader)
     expect(response.config.headers['User-Agent']).toBe(userAgentHeader)
+  })
+
+  it('client passes default headers to POST request', async () => {
+    const network = 'helium'
+    const nameHeader = 'name-header-test'
+    const userAgentHeader = 'user-agent-test'
+    nock('https://api.helium.io').post('/v1/greeting', { greeting: 'hello' }).reply(200, {
+      response: 'hey there!',
+    })
+    const opts = { name: nameHeader, userAgent: userAgentHeader, headers: { network } }
+    const client = new Client(Network.production, opts)
+    const params = { greeting: 'hello' }
+    const response = await client.post('/greeting', params)
+
+    expect(response.data.response).toBe('hey there!')
+    expect(response.config.headers['x-client-name']).toBe(nameHeader)
+    expect(response.config.headers['User-Agent']).toBe(userAgentHeader)
+    expect(response.config.headers.network).toBe(network)
   })
 })
 
