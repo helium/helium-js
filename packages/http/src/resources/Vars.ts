687d68903bd713af3818982782d501cadb563aab
Tyler Whitman <twhit093@gmail.com>
2021-11-03 11:38:05 -0700

    fetch only specific chain vars

--- packages/http/src/resources/Vars.ts
+++ packages/http/src/resources/Vars.ts
@@ -1,16 +1,35 @@
-import type Client from '../Client'
 import camelcaseKeys from 'camelcase-keys'
+import type Client from '../Client'
 
 export default class Vars {
   private client!: Client
+
+  /**
+   * The chain vars needed for transaction configuration.
+   * @private
+   */
+  private TXN_VARS: string[] = [
+    'txn_fee_multiplier',
+    'dc_payload_size',
+    'staking_fee_txn_assert_location_v1',
+    'staking_fee_txn_add_gateway_v1',
+  ]
 
   constructor(client: Client) {
     this.client = client
   }
 
-  async get(): Promise<any> {
-    const url = `/vars`
-    const { data: { data: vars } } = await this.client.get(url)
+  /**
+   * Fetches chain vars passed in keys and default includes the {@link TXN_VARS}:
+   * 'txn_fee_multiplier'
+   * 'dc_payload_size'
+   * 'staking_fee_txn_assert_location_v1'
+   * 'staking_fee_txn_add_gateway_v1'
+   * @param keys array of extra chain vars to fetch eg 'poc_v4_prob_no_rssi'
+   */
+  async get(keys: string[] = []): Promise<any> {
+    const url = '/vars'
+    const { data: { data: vars } } = await this.client.get(url, { keys: [...this.TXN_VARS, ...keys].join(',') })
     return camelcaseKeys(vars)
   }
 }
--- packages/http/src/resources/__tests__/Vars.spec.ts
+++ packages/http/src/resources/__tests__/Vars.spec.ts
@@ -1,36 +1,54 @@
 import nock from 'nock'
 import Client from '../../Client'
+import MOCK_CHAIN_VARS from '../__mocks__/varsMock'
 
-const varsFixture = () => ({
-  election_seen_penalty: 0.0033333,
-  election_removal_pct: 40,
-  price_oracle_public_keys: [
-    '13Btezbvbwr9LhKmDQLgBnJUgjhZighEjNPLeu79dqBbmXRwoWm',
-    '13CFFcmPtMvNQCpWQRXCTqXPnXtcsibDWVwiQRKpUCt4nqtF7RE',
-    '1431WVQvoV7RAJpoLCaBrTKner1Soed4bk69DddcrHUTCWHV6pj',
-    '136n9BEbreGUNgXJWtyzkBQcXiNzdMQ5GBoP8L2J6ZReFUAwUjy',
-    '14sqAYg1qxzjKTtyHLYZdH6yDtA3KgyoARhWN1cvLZ94dZw5vEc',
-    '145J6Aye86pKTJrUHREiXu7qqppZBcWY1bvWo8id7ZjxyuainYj',
-    '13dUGHis1PdrSwxdseoyZKzQhc8WuWcueAWdT95sDVGDNhGRWV9',
-    '14EzXp4i1xYA7SNyim6R4J5aXN1yHYKNiPrrJ2WEvoDnxmLgaCg',
-    '147yRbowD1krUCC1DhhSMhpFEqnkwb26mHBow5nk9q43AakSHNA',
-  ],
-  max_xor_filter_size: 102400,
-  max_subnet_num: 5,
-  min_subnet_size: 8,
-})
+const varsFixture = (keys: string[] = []) => {
+  const vars = JSON.parse(MOCK_CHAIN_VARS)
+  return Object.keys(vars)
+    .filter((key) => keys.includes(key))
+    .reduce((res, key) => Object.assign(res, { [key]: vars[key] }), {})
+}
 
 describe('get vars', () => {
-  nock('https://api.helium.io').get('/v1/vars').reply(200, {
-    data: varsFixture(),
-  })
+  nock('https://api.helium.io')
+    .get('/v1/vars?keys=txn_fee_multiplier%2Cdc_payload_size%2Cstaking_fee_txn_assert_location_v1%2Cstaking_fee_txn_add_gateway_v1')
+    .reply(200, {
+      data: varsFixture([
+        'txn_fee_multiplier',
+        'dc_payload_size',
+        'staking_fee_txn_assert_location_v1',
+        'staking_fee_txn_add_gateway_v1',
+      ]),
+    })
 
-  it('retrieves chain vars', async () => {
+  nock('https://api.helium.io')
+    .get('/v1/vars?keys=txn_fee_multiplier%2Cdc_payload_size%2Cstaking_fee_txn_assert_location_v1%2Cstaking_fee_txn_add_gateway_v1%2Cpoc_v4_prob_no_rssi')
+    .reply(200, {
+      data: varsFixture([
+        'txn_fee_multiplier',
+        'dc_payload_size',
+        'staking_fee_txn_assert_location_v1',
+        'staking_fee_txn_add_gateway_v1',
+        'poc_v4_prob_no_rssi',
+      ]),
+    })
+
+  it('retrieves txn chain vars by default', async () => {
     const client = new Client()
     const vars = await client.vars.get()
-    expect(vars.electionRemovalPct).toBe(40)
-    expect(vars.priceOraclePublicKeys[0]).toBe(
-      '13Btezbvbwr9LhKmDQLgBnJUgjhZighEjNPLeu79dqBbmXRwoWm',
-    )
+    expect(vars.txnFeeMultiplier).toBe(5000)
+    expect(vars.dcPayloadSize).toBe(24)
+    expect(vars.stakingFeeTxnAssertLocationV1).toBe(1000000)
+    expect(vars.stakingFeeTxnAddGatewayV1).toBe(4000000)
+  })
+
+  it('retrieves txn chain vars and extra', async () => {
+    const client = new Client()
+    const vars = await client.vars.get(['poc_v4_prob_no_rssi'])
+    expect(vars.txnFeeMultiplier).toBe(5000)
+    expect(vars.dcPayloadSize).toBe(24)
+    expect(vars.stakingFeeTxnAssertLocationV1).toBe(1000000)
+    expect(vars.stakingFeeTxnAddGatewayV1).toBe(4000000)
+    expect(vars.pocV4ProbNoRssi).toBe(0.5)
   })
 })
