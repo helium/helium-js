b4de2e6c9b16e2d12941159befc140ded31561f8
Tyler Whitman <twhit093@gmail.com>
2021-11-03 15:25:28 -0700

    review changes

--- packages/http/src/resources/Vars.ts
+++ packages/http/src/resources/Vars.ts
@@ -1,6 +1,13 @@
 import camelcaseKeys from 'camelcase-keys'
 import type Client from '../Client'
 
+interface TransactionVars {
+  txnFeeMultiplier?: number
+  dcPayloadSize?: number
+  stakingFeeTxnAssertLocationV1?: number
+  stakingFeeTxnAddGatewayV1?: number
+}
+
 export default class Vars {
   private client!: Client
 
@@ -15,21 +22,39 @@ export default class Vars {
     'staking_fee_txn_add_gateway_v1',
   ]
 
+  private baseUrl = '/vars'
+
   constructor(client: Client) {
     this.client = client
   }
 
   /**
-   * Fetches chain vars passed in keys and default includes the {@link TXN_VARS}:
-   * 'txn_fee_multiplier'
-   * 'dc_payload_size'
-   * 'staking_fee_txn_assert_location_v1'
-   * 'staking_fee_txn_add_gateway_v1'
-   * @param keys array of extra chain vars to fetch eg 'poc_v4_prob_no_rssi'
+   * Fetches the chain vars required to configure helium-js for blockchain transactions.
+   */
+  async getTransactionVars(): Promise<TransactionVars> {
+    const { data: { data: vars } } = await this.client.get(this.baseUrl, { keys: this.TXN_VARS.join(',') })
+    return camelcaseKeys(vars)
+  }
+
+  /**
+   * Fetches the specific chain vars passed in keys. If called without keys it will fetch the
+   * transaction vars returned by {@link getTransactionVars}.
+   * @param keys array of chain vars to fetch eg 'poc_v4_prob_no_rssi'
    */
   async get(keys: string[] = []): Promise<any> {
-    const url = '/vars'
-    const { data: { data: vars } } = await this.client.get(url, { keys: [...this.TXN_VARS, ...keys].join(',') })
+    if (keys.length === 0) {
+      return this.getTransactionVars()
+    }
+    const { data: { data: vars } } = await this.client.get(this.baseUrl, { keys: keys.join(',') })
+    return camelcaseKeys(vars)
+  }
+
+  /**
+   * WARNING: This will be return over 10 MB of data.
+   * Fetches all chain vars.
+   */
+  async getAll(): Promise<any> {
+    const { data: { data: vars } } = await this.client.get(this.baseUrl)
     return camelcaseKeys(vars)
   }
 }
--- packages/http/src/resources/__tests__/Vars.spec.ts
+++ packages/http/src/resources/__tests__/Vars.spec.ts
@@ -4,12 +4,21 @@ import MOCK_CHAIN_VARS from '../__mocks__/varsMock'
 
 const varsFixture = (keys: string[] = []) => {
   const vars = JSON.parse(MOCK_CHAIN_VARS)
+  if (keys.length === 0) {
+    return vars
+  }
   return Object.keys(vars)
     .filter((key) => keys.includes(key))
     .reduce((res, key) => Object.assign(res, { [key]: vars[key] }), {})
 }
 
 describe('get vars', () => {
+  nock('https://api.helium.io')
+    .get('/v1/vars')
+    .reply(200, {
+      data: varsFixture(),
+    })
+
   nock('https://api.helium.io')
     .get('/v1/vars?keys=txn_fee_multiplier%2Cdc_payload_size%2Cstaking_fee_txn_assert_location_v1%2Cstaking_fee_txn_add_gateway_v1')
     .reply(200, {
@@ -22,33 +31,50 @@ describe('get vars', () => {
     })
 
   nock('https://api.helium.io')
-    .get('/v1/vars?keys=txn_fee_multiplier%2Cdc_payload_size%2Cstaking_fee_txn_assert_location_v1%2Cstaking_fee_txn_add_gateway_v1%2Cpoc_v4_prob_no_rssi')
+    .get('/v1/vars?keys=poc_v4_prob_no_rssi')
     .reply(200, {
       data: varsFixture([
-        'txn_fee_multiplier',
-        'dc_payload_size',
-        'staking_fee_txn_assert_location_v1',
-        'staking_fee_txn_add_gateway_v1',
         'poc_v4_prob_no_rssi',
       ]),
     })
 
-  it('retrieves txn chain vars by default', async () => {
+  nock('https://api.helium.io')
+    .get('/v1/vars?keys=poc_v4_prob_no_rssi%2Cpoc_addr_hash_byte_count')
+    .reply(200, {
+      data: varsFixture([
+        'poc_v4_prob_no_rssi',
+        'poc_addr_hash_byte_count',
+      ]),
+    })
+
+  it('getAll retrieves all chain vars', async () => {
+    const client = new Client()
+    const vars = await client.vars.getAll()
+    expect(Object.keys(vars).length).toBe(158)
+  })
+
+  it('get retrieves txn chain vars by default', async () => {
     const client = new Client()
     const vars = await client.vars.get()
     expect(vars.txnFeeMultiplier).toBe(5000)
     expect(vars.dcPayloadSize).toBe(24)
     expect(vars.stakingFeeTxnAssertLocationV1).toBe(1000000)
     expect(vars.stakingFeeTxnAddGatewayV1).toBe(4000000)
+    expect(Object.keys(vars).length).toBe(4)
   })
 
-  it('retrieves txn chain vars and extra', async () => {
+  it('get retrieves one specific chain var', async () => {
     const client = new Client()
     const vars = await client.vars.get(['poc_v4_prob_no_rssi'])
-    expect(vars.txnFeeMultiplier).toBe(5000)
-    expect(vars.dcPayloadSize).toBe(24)
-    expect(vars.stakingFeeTxnAssertLocationV1).toBe(1000000)
-    expect(vars.stakingFeeTxnAddGatewayV1).toBe(4000000)
     expect(vars.pocV4ProbNoRssi).toBe(0.5)
+    expect(Object.keys(vars).length).toBe(1)
+  })
+
+  it('get retrieves multiple specific chain vars', async () => {
+    const client = new Client()
+    const vars = await client.vars.get(['poc_v4_prob_no_rssi', 'poc_addr_hash_byte_count'])
+    expect(vars.pocV4ProbNoRssi).toBe(0.5)
+    expect(vars.pocAddrHashByteCount).toBe(8)
+    expect(Object.keys(vars).length).toBe(2)
   })
 })
