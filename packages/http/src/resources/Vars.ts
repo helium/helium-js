b9cb8c0630577893c01709b99bcec55af951d4a0
Tyler Whitman <twhit093@gmail.com>
2021-11-04 07:30:47 -0700

    fetch only specific chain vars (#254)
    * fetch only specific chain vars
    
    * review changes

--- packages/http/src/resources/Vars.ts
+++ packages/http/src/resources/Vars.ts
@@ -1,16 +1,60 @@
-import type Client from '../Client'
 import camelcaseKeys from 'camelcase-keys'
+import type Client from '../Client'
+
+interface TransactionVars {
+  txnFeeMultiplier?: number
+  dcPayloadSize?: number
+  stakingFeeTxnAssertLocationV1?: number
+  stakingFeeTxnAddGatewayV1?: number
+}
 
 export default class Vars {
   private client!: Client
+
+  /**
+   * The chain vars needed for transaction configuration.
+   * @private
+   */
+  private TXN_VARS: string[] = [
+    'txn_fee_multiplier',
+    'dc_payload_size',
+    'staking_fee_txn_assert_location_v1',
+    'staking_fee_txn_add_gateway_v1',
+  ]
+
+  private baseUrl = '/vars'
 
   constructor(client: Client) {
     this.client = client
   }
 
-  async get(): Promise<any> {
-    const url = `/vars`
-    const { data: { data: vars } } = await this.client.get(url)
+  /**
+   * Fetches the chain vars required to configure helium-js for blockchain transactions.
+   */
+  async getTransactionVars(): Promise<TransactionVars> {
+    const { data: { data: vars } } = await this.client.get(this.baseUrl, { keys: this.TXN_VARS.join(',') })
+    return camelcaseKeys(vars)
+  }
+
+  /**
+   * Fetches the specific chain vars passed in keys. If called without keys it will fetch the
+   * transaction vars returned by {@link getTransactionVars}.
+   * @param keys array of chain vars to fetch eg 'poc_v4_prob_no_rssi'
+   */
+  async get(keys: string[] = []): Promise<any> {
+    if (keys.length === 0) {
+      return this.getTransactionVars()
+    }
+    const { data: { data: vars } } = await this.client.get(this.baseUrl, { keys: keys.join(',') })
+    return camelcaseKeys(vars)
+  }
+
+  /**
+   * WARNING: This will be return over 10 MB of data.
+   * Fetches all chain vars.
+   */
+  async getAll(): Promise<any> {
+    const { data: { data: vars } } = await this.client.get(this.baseUrl)
     return camelcaseKeys(vars)
   }
 }
--- packages/http/src/resources/__tests__/Vars.spec.ts
+++ packages/http/src/resources/__tests__/Vars.spec.ts
@@ -1,36 +1,80 @@
 import nock from 'nock'
 import Client from '../../Client'
+import MOCK_CHAIN_VARS from '../__mocks__/varsMock'
 
-const varsFixture = () => ({
-  election_seen_penalty: 0.0033333,
-  election_removal_pct: 40,
-  price_oracle_public_keys: [
-    '13Btezbvbwr9LhKmDQLgBnJUgjhZighEjNPLeu79dqBbmXRwoWm',
-    '13CFFcmPtMvNQCpWQRXCTqXPnXtcsibDWVwiQRKpUCt4nqtF7RE',
-    '1431WVQvoV7RAJpoLCaBrTKner1Soed4bk69DddcrHUTCWHV6pj',
-    '136n9BEbreGUNgXJWtyzkBQcXiNzdMQ5GBoP8L2J6ZReFUAwUjy',
-    '14sqAYg1qxzjKTtyHLYZdH6yDtA3KgyoARhWN1cvLZ94dZw5vEc',
-    '145J6Aye86pKTJrUHREiXu7qqppZBcWY1bvWo8id7ZjxyuainYj',
-    '13dUGHis1PdrSwxdseoyZKzQhc8WuWcueAWdT95sDVGDNhGRWV9',
-    '14EzXp4i1xYA7SNyim6R4J5aXN1yHYKNiPrrJ2WEvoDnxmLgaCg',
-    '147yRbowD1krUCC1DhhSMhpFEqnkwb26mHBow5nk9q43AakSHNA',
-  ],
-  max_xor_filter_size: 102400,
-  max_subnet_num: 5,
-  min_subnet_size: 8,
-})
+const varsFixture = (keys: string[] = []) => {
+  const vars = JSON.parse(MOCK_CHAIN_VARS)
+  if (keys.length === 0) {
+    return vars
+  }
+  return Object.keys(vars)
+    .filter((key) => keys.includes(key))
+    .reduce((res, key) => Object.assign(res, { [key]: vars[key] }), {})
+}
 
 describe('get vars', () => {
-  nock('https://api.helium.io').get('/v1/vars').reply(200, {
-    data: varsFixture(),
+  nock('https://api.helium.io')
+    .get('/v1/vars')
+    .reply(200, {
+      data: varsFixture(),
+    })
+
+  nock('https://api.helium.io')
+    .get('/v1/vars?keys=txn_fee_multiplier%2Cdc_payload_size%2Cstaking_fee_txn_assert_location_v1%2Cstaking_fee_txn_add_gateway_v1')
+    .reply(200, {
+      data: varsFixture([
+        'txn_fee_multiplier',
+        'dc_payload_size',
+        'staking_fee_txn_assert_location_v1',
+        'staking_fee_txn_add_gateway_v1',
+      ]),
+    })
+
+  nock('https://api.helium.io')
+    .get('/v1/vars?keys=poc_v4_prob_no_rssi')
+    .reply(200, {
+      data: varsFixture([
+        'poc_v4_prob_no_rssi',
+      ]),
+    })
+
+  nock('https://api.helium.io')
+    .get('/v1/vars?keys=poc_v4_prob_no_rssi%2Cpoc_addr_hash_byte_count')
+    .reply(200, {
+      data: varsFixture([
+        'poc_v4_prob_no_rssi',
+        'poc_addr_hash_byte_count',
+      ]),
+    })
+
+  it('getAll retrieves all chain vars', async () => {
+    const client = new Client()
+    const vars = await client.vars.getAll()
+    expect(Object.keys(vars).length).toBe(158)
   })
 
-  it('retrieves chain vars', async () => {
+  it('get retrieves txn chain vars by default', async () => {
     const client = new Client()
     const vars = await client.vars.get()
-    expect(vars.electionRemovalPct).toBe(40)
-    expect(vars.priceOraclePublicKeys[0]).toBe(
-      '13Btezbvbwr9LhKmDQLgBnJUgjhZighEjNPLeu79dqBbmXRwoWm',
-    )
+    expect(vars.txnFeeMultiplier).toBe(5000)
+    expect(vars.dcPayloadSize).toBe(24)
+    expect(vars.stakingFeeTxnAssertLocationV1).toBe(1000000)
+    expect(vars.stakingFeeTxnAddGatewayV1).toBe(4000000)
+    expect(Object.keys(vars).length).toBe(4)
+  })
+
+  it('get retrieves one specific chain var', async () => {
+    const client = new Client()
+    const vars = await client.vars.get(['poc_v4_prob_no_rssi'])
+    expect(vars.pocV4ProbNoRssi).toBe(0.5)
+    expect(Object.keys(vars).length).toBe(1)
+  })
+
+  it('get retrieves multiple specific chain vars', async () => {
+    const client = new Client()
+    const vars = await client.vars.get(['poc_v4_prob_no_rssi', 'poc_addr_hash_byte_count'])
+    expect(vars.pocV4ProbNoRssi).toBe(0.5)
+    expect(vars.pocAddrHashByteCount).toBe(8)
+    expect(Object.keys(vars).length).toBe(2)
   })
 })
