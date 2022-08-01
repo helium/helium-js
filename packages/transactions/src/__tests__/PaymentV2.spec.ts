6f04af61dbd11d594af0ce9190ebbf3b39bafbca
Matt Reetz <matthewcarlreetz@gmail.com>
2022-08-01 14:13:16 -0500

    Add additional tests.

--- packages/transactions/src/__tests__/PaymentV2.spec.ts
+++ packages/transactions/src/__tests__/PaymentV2.spec.ts
@@ -26,6 +26,23 @@ const paymentFixture = async () => {
   })
 }
 
+const mobilePaymentFixture = async () => {
+  const { bob, alice } = await usersFixture()
+
+  return new PaymentV2({
+    payer: bob.address,
+    payments: [
+      {
+        payee: alice.address,
+        amount: 10,
+        memo: 'bW9ja21lbW8=',
+        tokenType: 'mobile',
+      },
+    ],
+    nonce: 1,
+  })
+}
+
 test('create a PaymentV2', async () => {
   const payment = await paymentFixture()
 
@@ -64,7 +81,7 @@ describe('serialize and deserialize', () => {
     expect(deserialized.payments[0]?.amount).toBe(payment.payments[0]?.amount)
     expect(deserialized.payments[0]?.payee.b58).toBe(payment.payments[0]?.payee.b58)
     expect(deserialized.payments[0]?.memo).toBe(payment.payments[0]?.memo)
-    expect(deserialized.payments[0]?.tokenType).toBe(payment.payments[0]?.tokenType)
+    expect(deserialized.payments[0]?.tokenType).toBe('hnt')
     expect(deserialized.fee).toBe(payment.fee)
   })
 })
@@ -109,6 +126,35 @@ describe('sign', () => {
     expect(deserializedPayment.signature).toEqual(signedPayment.signature)
   })
 
+  it("has the same signature when specifying token type 'hnt' as when omitting", async () => {
+    const { bob, alice } = await usersFixture()
+    const paymentWithoutTokenType = new PaymentV2({
+      payer: bob.address,
+      payments: [
+        {
+          payee: alice.address,
+          amount: 10,
+        },
+      ],
+      nonce: 1,
+    })
+
+    const paymentWithTokenType = new PaymentV2({
+      payer: bob.address,
+      payments: [
+        {
+          payee: alice.address,
+          amount: 10,
+        },
+      ],
+      nonce: 1,
+    })
+
+    const signedPaymentWithout = await paymentWithoutTokenType.sign({ payer: bob })
+    const signedPaymentWith = await paymentWithTokenType.sign({ payer: bob })
+    expect(signedPaymentWithout.toString()).toEqual(signedPaymentWith.toString())
+  })
+
   it('should sign the correct signature while handling an empty memo', async () => {
     const payment = await paymentFixture()
     payment.payments[0].memo = 'AAAAAAAAAAA='
@@ -122,5 +168,21 @@ describe('sign', () => {
     expect(base64Signature).toEqual(
       'TKUrksvBBTjgjoPEPhFc79+jPzaVN7VBWgKo4+GvDYZ4BO9HujpEuskMSvzVWq0T/tf2KAKd6d+fT3mYnMRYCA==',
     )
+  })
+
+  it('should decode hnt token type to hnt', async () => {
+    const payment = await paymentFixture()
+    const { bob } = await usersFixture()
+    const signedPayment = await payment.sign({ payer: bob })
+    const deserialized = PaymentV2.fromString(signedPayment.toString())
+    expect(deserialized.payments?.[0].tokenType).toEqual('hnt')
+  })
+
+  it('should decoded mobile token type to mobile', async () => {
+    const payment = await mobilePaymentFixture()
+    const { bob } = await usersFixture()
+    const signedPayment = await payment.sign({ payer: bob })
+    const deserialized = PaymentV2.fromString(signedPayment.toString())
+    expect(deserialized.payments?.[0].tokenType).toEqual('mobile')
   })
 })
